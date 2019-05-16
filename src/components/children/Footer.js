b7b4106506232890e318440f68cf408914fa534d
crazyRubix <muneebkt@gmail.com>
2019-05-17 03:19:15 +0530

    add memo to footer

--- src/components/children/Footer.js
+++ src/components/children/Footer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import Memo from './Memo'
 class Footer extends Component {
     render() {
         var copyrightStr = "© COPYRIGHT PISERVE TECHNOLOGIES PVT. LTD 2019. ALL RIGHTS RESERVED.";
@@ -9,6 +9,7 @@ class Footer extends Component {
                 <h5>Muneeb Mukhtar</h5>               
                 <label style={{float:'right'}}>Powered by Piserve @2019</label><br/>
                 <label style={{float:'left'}}>{copyright}</label>
+                <Memo />
                 <label style={{float:'right'}}>React : <strong>{React.version}</strong></label>
                 </div>
         );
---
+++ src/components/children/MemoChild.js
@@ -0,0 +1,12 @@
+import React  from 'react';
+const MemoChild = props => {
+    console.log("I am rendered");
+    return (
+        <div>
+            <label>And am rendering every {props.seconds} seconds</label>
+        </div>
+    );
+};
+
+export default React.memo(MemoChild);
+
